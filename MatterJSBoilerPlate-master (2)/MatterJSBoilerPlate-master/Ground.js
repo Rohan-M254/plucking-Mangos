f753557467650e4ffc6ea07091fb34bfc6cf81e2
Rohan-M254 <71419752+Rohan-M254@users.noreply.github.com>
2020-10-19 22:20:18 -0500

    Add files via upload

---
+++ MatterJSBoilerPlate-master (2)/MatterJSBoilerPlate-master/Ground.js
@@ -0,0 +1,22 @@
+class Ground{
+	constructor (x,y,w,h){
+		var options = {
+		isStatic:true
+		}
+		this.x = x
+		this.y = y
+		this.w = w
+		this.h = h
+		this.body = Bodies.rectangle(x,y,w,h,options)
+		World.add(world,this.body)
+	}
+	display(){
+	var GroundPos = this.body.position
+	push();
+	translate(GroundPos.x,GroundPos.y)
+	rectMode(CENTER)
+	fill(255,0,255)
+	rect(0,0,this.w,this.h)
+	pop();
+	}
+}
